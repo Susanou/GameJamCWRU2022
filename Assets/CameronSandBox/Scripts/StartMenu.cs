ff710654ace55ef20a1b359e0bca0a3d53959cfa
Fukurou <caos2804@gmail.com>
2022-04-01 11:49:23 -0400

    Start and credit scenes

---
+++ Assets/CameronSandBox/Scripts/StartMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject MenuMusic;
+
+    public void startGame()
+    {
+        SceneManager.LoadScene(1); // 1 should be the number of the castle scene
+        Time.timeScale = 1f;
+        MenuMusic.SetActive(false);
+    }
+
+    public void credits()
+    {
+        SceneManager.LoadScene(2);
+        MenuMusic.SetActive(false);
+    }
+
+    public void startMenu()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        SceneManager.LoadScene(0); // 1 should be the number of the castle scene
+        MenuMusic.SetActive(false);
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    private void EndScene(){
+        SceneManager.LoadScene("End Scene");
+    }
+}
